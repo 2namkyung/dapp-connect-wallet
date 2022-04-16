76aee1d7e071d94aa1fbe7d6a7261b0d09779c82
2namkyung <iddbxk@daum.net>
2022-04-16 16:39:33 +0900

    redux-dev-tools

--- src/redux/create.js
+++ src/redux/create.js
@@ -1,19 +1,20 @@
-import createSagaMiddleware from "@redux-saga/core"
-import { applyMiddleware, createStore } from "redux"
-import reducer from "./modules/reducers"
-import rootSaga from "./modules/rootSaga"
+import createSagaMiddleware from "@redux-saga/core";
+import { applyMiddleware, createStore } from "redux";
+import reducer from "./modules/reducers";
+import rootSaga from "./modules/rootSaga";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const create = () =>{
-    const sagaMiddleware = createSagaMiddleware()
+const create = () => {
+  const sagaMiddleware = createSagaMiddleware();
 
-    const store = createStore(
-        reducer,
-        applyMiddleware(sagaMiddleware)
-    )
+  const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(sagaMiddleware))
+  );
 
-    sagaMiddleware.run(rootSaga)
+  sagaMiddleware.run(rootSaga);
 
-    return store
-}
+  return store;
+};
 
-export default create
+export default create;
